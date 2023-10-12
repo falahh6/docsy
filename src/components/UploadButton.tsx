325c2a740fd19d116b3d843e65c9dd79ac7e153f
Mohammed Falah <falahsss900@gmail.com>
2023-10-12 22:12:10 +0530

    creating pdfRender and chat support functionality, mods to upload via dropzone

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,15 +1,64 @@
 "use client";
 
 import { useState } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
+import Dropzone from "react-dropzone";
+import { Cloud, FileIcon } from "lucide-react";
+import { Progress } from "./ui/progress";
+
+const UploadDropzone = () => {
+  const [isUploading, setIsUploading] = useState<boolean>(true);
+
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFiles) => {
+        console.log(acceptedFiles);
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="h-64 m-4 border border-dashed border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center w-full h-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to Upload</span> or drag
+                  or drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (upto 4MB)</p>
+              </div>
+
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs rounded-md bg-white flex items-center overflow-hidden outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <FileIcon className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-2 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress value={20} className="h-1 w-full bg-zinc-300" />
+                </div>
+              ) : null}
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
 
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -20,9 +69,7 @@ const UploadButton = () => {
         <Button>Upload PDF</Button>
       </DialogTrigger>
       <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Drag your file here</DialogTitle>
-        </DialogHeader>
+        <UploadDropzone />
       </DialogContent>
     </Dialog>
   );
