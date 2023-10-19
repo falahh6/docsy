0ba56006779bd4f5d0b0bc2c029f2ce1c73848e1
Mohammed Falah <falahsss900@gmail.com>
2023-10-19 22:17:16 +0530

    add : opstimistic updates, infiniteQuery.

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -95,6 +95,77 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
           prevMessages?.pages.flatMap((pages) => pages.messages) ?? [],
       };
     },
+    onSuccess: async (stream) => {
+      setIsLoading(false);
+
+      if (!stream) {
+        return toast({
+          title: "There was an error sending this message",
+          description: "Please refresh this page and Try again",
+          variant: "destructive",
+        });
+      }
+
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      let accResponse = "";
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+
+        const chunkValue = decoder.decode(value);
+
+        accResponse += chunkValue;
+
+        utils.getFileMessages.setInfiniteData(
+          { fileId, limit: INFINITE_QUERY_LIMIT },
+          (old) => {
+            if (!old) return { pages: [], pageParams: [] };
+
+            let isAiResponseCreated = old.pages.some((page) =>
+              page.messages.some((message) => message.id === "ai-response")
+            );
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages;
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toISOString(),
+                      id: "ai-response",
+                      text: accResponse,
+                      isUserMessage: false,
+                    },
+                    ...page.messages,
+                  ];
+                } else {
+                  updatedMessages = page.messages.map((message) => {
+                    if (message.id === "ai-response") {
+                      return {
+                        ...message,
+                        text: accResponse,
+                      };
+                    }
+                    return message;
+                  });
+                }
+                return {
+                  ...page,
+                  messages: updatedMessages,
+                };
+              }
+              return page;
+            });
+            return { ...old, pages: updatedPages };
+          }
+        );
+      }
+    },
     onError: (_, __, context) => {
       setMessage(backupMessage.current);
 
